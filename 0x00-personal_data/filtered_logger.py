400e197148fb7ef798092f34b204c17ab6f33c70
emmanueladdo <asememmanueladd@gmail.com>
2024-01-11 16:37:33 +0000

    regexing0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ Function returns the Log Messages
 import re
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """
