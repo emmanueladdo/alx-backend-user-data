b5ae8cbdf9cee6fea8a79904eb4e5c6220cd17e1
emmanueladdo <asememmanueladd@gmail.com>
2024-01-18 22:04:02 +0000

    sessions ad

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -67,7 +67,7 @@ def before_request() -> str:
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if (not auth.authorization_header(request) and
