fb5042b4764716248149d4eb1d60d59be43b9676
emmanueladdo <asememmanueladd@gmail.com>
2024-01-18 20:42:02 +0000

    file push

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+manages the API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    Class that defines the authenication of API
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """authorithation requirement check"""
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for p in excluded_paths:
+            if p.endswith('*'):
+                if path.startswith(p[:1]):
+                    return False
+        return False if path in excluded_paths else True
+
+    def authorization_header(self, request=None) -> str:
+        """header check"""
+        if request:
+            return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user"""
+        return None
