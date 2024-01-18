ff9ae9e64da065fa503271ca5c883da2da749d0b
emmanueladdo <asememmanueladd@gmail.com>
2024-01-18 21:40:48 +0000

    sessions

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -72,6 +72,8 @@ def before_request() -> str:
         return
     if auth.authorization_header(request) is None:
         abort(401)
+    if auth.authorization_header is not auth.session_cookie(request):
+        return (401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
